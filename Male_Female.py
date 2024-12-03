5499f67afcfec0f9a59a918e4ef7c7ac40a6b17b
Roopa-G95 <roopagokulakrishnan95@gmail.com>
2024-12-03 10:41:10 +0530

    Gender

---
+++ Male_Female.py
@@ -0,0 +1,7 @@
+a=str(input('Enter your Gender'))
+if(a.lower()=="male"):
+    print("You are a Male")
+elif(a.lower()=="female"):
+    print("You are a Female")
+else:
+    print("Please enter the correct value and check the spelling")
